10bc66c005f2a315680f48f1144ad8a91b326bbf
jeffrey.fontanilla <jeffrey.fontanilla@89DS262>
2017-12-27 17:28:20 +0800

    aaa

--- application/views/header.php
+++ application/views/header.php
@@ -73,20 +73,60 @@
             <div class="collapse navbar-collapse center" id="main-nav-collapse">
                 <ul class="nav navbar-nav navbar-right  text-uppercase">
                     <li>
-                        <a href="<?php echo base_url('/home'); ?>"><span>home</span></a>
+                        <a href="index.html"><span>home</span></a>
                     </li>
+
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span>pages</span></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="about.html">about</a>
+                            </li>
+                            <li>
+                                <a href="service.html">service</a>
+                            </li>
+                            <li>
+                                <a href="portfolio.html">portfolio</a>
+                            </li>
+                            <li>
+                                <a href="gallery.html">gallery</a>
+                            </li>
+                            <li>
+                                <a href="404.html">404 page</a>
+                            </li>
+                            <li>
+                                <a href="coming_soon.html">coming soon</a>
+                            </li>
+                        </ul>  <!-- end of /.dropdown-menu -->
+                    </li> <!-- end of /.dropdown -->
+
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span>blog</span></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="multiple_blog_left_sidebar.html">multiple with left sidebar</a>
+                            </li>
+                            <li>
+                                <a href="multiple_blog.html">multiple with right sidebar</a>
+                            </li>
+                            <li>
+                                <a href="single_blog_left_sidebar.html">single with left sidebar</a>
+                            </li>
+                            <li>
+                                <a href="single_blog.html">single with right sidebar</a>
+                            </li>
+                            <li>
+                                <a href="single_blog_full_width.html">single full width</a>
+                            </li>
+                        </ul> <!-- end of /.dropdown-menu -->
+                    </li> <!-- end of /.dropdown -->
 
                     <li>
-                        <a href="#"><span>messages</span></a>
+                        <a href="contact.html"><span>contact</span></a>
                     </li>
-
                     <li>
-                        <a href="<?php echo base_url('/profile'); ?>"><span>profile</span></a>
-                    </li>
-
-                    <!-- <li>
                         <?= $alumnusData->first_name ?>
-                    </li> -->
+                    </li>
                 </ul>
             </div><!-- nav links -->
             
